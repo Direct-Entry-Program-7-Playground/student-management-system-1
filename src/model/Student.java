f7c53b79e664550efa8d230c913fa7250a33f448
Manoj Randeni <randeni.ramr@gmail.com>
2021-06-11 00:18:25 +0530

    Create Student class and StudentService class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,31 @@
 package model;
 
 public class Student {
+    private String nic;
+    private String fullName;
+    private String address;
 
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -2,32 +2,55 @@ package service;
 
 import model.Student;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentService {
 
-    public void saveStudent(Student student){
+    List<Student> students = new ArrayList<>();
 
+    public void saveStudent(Student student) {
+        students.add(student);
     }
 
-    public void updateStudent(Student student){
-
+    public void updateStudent(Student student) {
+        for (Student studentFromList :
+                students) {
+            if (studentFromList.getNic().equals(student.getNic())) {
+                deleteStudent(student.getNic());
+                students.add(student);
+            }
+        }
     }
 
-    public void deleteStudent(String studentId){
-
+    public void deleteStudent(String studentId) {
+        students.removeIf(student -> student.getNic().equals(studentId));
     }
 
-    public List<Student> findAllStudents(){
+    public List<Student> findAllStudents() {
+
+        return students;
+    }
+
+    public Student findStudent(String studentId) {
+        for (Student student : students
+        ) {
+            if (student.getNic().equals(studentId)) {
+                return student;
+            }
+        }
         return null;
     }
 
-    public Student findStudent(String studentId){
-        return null;
-    }
-
-    public List<Student> findStudents(String query){
-        return null;
+    public List<Student> findStudents(String query) {
+        List<Student> queriedStudents = new ArrayList<>();
+        for (Student student : students
+        ) {
+            if (student.getNic().contains(query) || student.getFullName().contains(query) || student.getAddress().contains(query)) {
+                queriedStudents.add(student);
+            }
+        }
+        return queriedStudents;
     }
 
 }
