f5cc49813c0234310601925a8732bac2ac10b566
Manoj Randeni <randeni.ramr@gmail.com>
2021-06-11 19:20:32 +0530

    Update the searchStudent form

--- src/controller/SearchStudentsFormController.java
+++ src/controller/SearchStudentsFormController.java
@@ -2,20 +2,38 @@ package controller;
 
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Student;
+import model.StudentTM;
 import service.StudentService;
 import util.MaterialUI;
+
+import java.util.List;
 
 public class SearchStudentsFormController {
 
     public TextField txtQuery;
-    public TableView tblResults;
+    public TableView<StudentTM> tblResults;
+
 
     private StudentService studentService = new StudentService();
 
-    public void initialize(){
+    public void initialize() {
         MaterialUI.paintTextFields(txtQuery);
-        tblResults.getItems().add(new Object());
-        tblResults.getItems().add(new Object());
-        tblResults.getItems().add(new Object());
+
+        tblResults.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("nic"));
+        tblResults.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("fullName"));
+        tblResults.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
+        loadAllStudents();
+    }
+
+    public void loadAllStudents() {
+        tblResults.getItems().clear();
+
+        List<Student> allStudents = studentService.findAllStudents();
+        for (Student student :
+                allStudents) {
+            tblResults.getItems().add(new StudentTM(student.getNic(), student.getFullName(), student.getAddress()));
+        }
     }
 }
--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -7,13 +7,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StudentService {
-    static {
-        StudentService studentService = new StudentService();
-        studentService.saveStudent(new Student("123456789V", "Chandimal", "Matara", "23161548", "chandi@email.com", LocalDate.now()));
-        studentService.saveStudent(new Student("485912632V", "PKP", "Matara", "48563217", "pkp@email.com", LocalDate.now()));
-        studentService.saveStudent(new Student("3576892125V", "MRR", "Kurunegala", "24576891", "mrr@email.com", LocalDate.now()));
-    }
+
     private static List<Student> studentDB = new ArrayList<>();
+
+    public StudentService() {
+        saveStudent(new Student("123456789V", "Chandimal", "Matara", "23161548", "chandi@email.com", LocalDate.now()));
+        saveStudent(new Student("485912632V", "PKP", "Matara", "48563217", "pkp@email.com", LocalDate.now()));
+        saveStudent(new Student("3576892125V", "MRR", "Kurunegala", "24576891", "mrr@email.com", LocalDate.now()));
+    }
 
     @Override
     public String toString() {
